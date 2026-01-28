b62efb9cd309cf00f50ffb03bf7033ca442b669b
Niranjan Barupal <niranjanbarupal182@gmail.com>
2026-01-28 23:15:42 +0530

    Fix Crash

--- src/Components/Dashboard/AdminDashboard.jsx
+++ src/Components/Dashboard/AdminDashboard.jsx
@@ -6,7 +6,6 @@ import CreateTask from '../Other/CreateTask'
 const AdminDashboard = ({changeUser,employees,setEmployees}) => {
   return (
     <div className='pl-5 pr-5 h-screen bg-slate-100'>
-        <h1 className='text-4xl text-slate-900 lg:text-5xl font-serif text-center font-semibold mb-5'>Admin Panel</h1>
 
         <Header changeUser={changeUser}/>
 
